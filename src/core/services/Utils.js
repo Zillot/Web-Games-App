7490d6fb56dc5d8fd7d002e73de2522bd732b5fd
firefoll06@gmail.com <perseker061>
2018-02-20 12:00:15 +0200

    random coments

--- src/core/services/Utils.js
+++ src/core/services/Utils.js
@@ -1,8 +1,10 @@
 class Utils {
+	//include min and max
 	randI(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
-	randF(min, max) {
+    //include min and max
+    randF(min, max) {
 		return Math.random() * (max - min) + min;
 	}
 	randColor() {
