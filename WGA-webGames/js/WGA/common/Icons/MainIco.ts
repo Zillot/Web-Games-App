2e119748768f171a57c6e13061662183daf4e2a6
Mykola <firefoll06@gmail.com>
2019-04-05 17:57:11 +0300

    Fixes to main ico draving

--- WGA-webGames/js/WGA/common/Icons/MainIco.ts
+++ WGA-webGames/js/WGA/common/Icons/MainIco.ts
@@ -18,11 +18,12 @@ module WGAAppModule {
         private pointsRight: Vector2[];
 
         constructor(position: Vector2, size: number, cubes: number, speed: number) {
+            this.leftRigthDir = new Vector2(1, 0);
             this.Position = position;
             this.speed = speed;
 
             this.proccess = new Value(0, speed);
-            this.startMoving();
+            this.startMoving(this);
 
             this.SetCubeData(size, cubes);
         }
@@ -48,25 +49,24 @@ module WGAAppModule {
             this.size = size;
 
             var oneCubeSize = size / cubes;
-            this.moveDistance = oneCubeSize;
+            this.moveDistance = oneCubeSize / 3;
 
             this.pointsTop = [
                 new Vector2(0, 0),
-                new Vector2(-oneCubeSize, -oneCubeSize / 2),
-                new Vector2(0, -oneCubeSize),
-                new Vector2(oneCubeSize, -oneCubeSize / 2),
+                new Vector2(-oneCubeSize / 2, -oneCubeSize / 2),
+                new Vector2(oneCubeSize / 2, -oneCubeSize / 2),
             ];
             this.pointsLeft = [
                 new Vector2(0, 0),
-                new Vector2(0, oneCubeSize),
-                new Vector2(-oneCubeSize, oneCubeSize / 2),
-                new Vector2(-oneCubeSize, -oneCubeSize / 2),
+                new Vector2(-oneCubeSize / 2, -oneCubeSize / 2),
+                new Vector2(-oneCubeSize / 2, oneCubeSize / 2),
+                new Vector2(0, oneCubeSize / 2),
             ];
-            this.pointsLeft = [
+            this.pointsRight = [
                 new Vector2(0, 0),
-                new Vector2(oneCubeSize, -oneCubeSize / 2),
-                new Vector2(oneCubeSize, oneCubeSize / 2),
-                new Vector2(0, oneCubeSize),
+                new Vector2(oneCubeSize / 2, -oneCubeSize / 2),
+                new Vector2(oneCubeSize / 2, oneCubeSize / 2),
+                new Vector2(0, oneCubeSize / 2),
             ];
         }
 
@@ -76,57 +76,53 @@ module WGAAppModule {
 
         public Draw(): void {
             var stepOffset = this.proccess.GetVal() * this.moveDistance;
-            var upDownDir = this.leftRigthDir.RotateTo(Math.PI / 2);
+            var upDownDir = Vector2.GetRotated(this.leftRigthDir, Math.PI / 2);
+            var oneCubeSize = this.size / this.cubes;
 
-            var position = this.getStartPosition(stepOffset, upDownDir)
+            var globalOffset = stepOffset * (Math.floor(this.cubes / 2));
+            var position = this.getStartPosition(globalOffset, upDownDir);
 
             for (var i = 0; i < this.cubes; i++) {
                 for (var j = 0; j < this.cubes; j++) {
                     //moving by X
                     var cubePosition = position
-                        .ADD(this.leftRigthDir
-                            .MUL(this.size * j * this.proccess.GetVal()))
-                        .ADD(stepOffset);
+                        .ADD(this.leftRigthDir.MUL(oneCubeSize * j + stepOffset * j - 1));
 
-                    this.drawCube(cubePosition);
+                    this.drawCube(cubePosition, 0.8);
                 }
 
                 //moving by Y
                 position = position
-                    .ADD(this.leftRigthDir.RotateTo(Math.PI / 2)
-                        .MUL(this.size * this.proccess.GetVal()))
-                    .ADD(stepOffset);
+                    .ADD(upDownDir.MUL(oneCubeSize + stepOffset));
             }
         }
 
-        private getStartPosition(stepOffset: number, upDownDir: Vector2) {
-            //moving by X to the left
-            var position = this.Position
-                .SUB(this.leftRigthDir
-                    .MUL(this.size * (this.cubes / 2)))
-                .SUB(stepOffset * ((this.cubes / 2) - 1));
+        private getStartPosition(globalOffset: number, upDownDir: Vector2) {
+            var oneCubeSize = this.size / this.cubes;
+            var position = this.Position.ADD(oneCubeSize / 2);
 
-            //moving by Y to the top
-            position = this.Position
-                .SUB(upDownDir
-                    .MUL(this.size * (this.cubes / 2) * this.proccess.GetVal()))
-                .SUB(stepOffset * ((this.cubes / 2) - 1));
+            var mover = new Vector2(this.leftRigthDir.X, upDownDir.Y);
+            position = position.SUB(mover.MUL(oneCubeSize * this.cubes / 2 + globalOffset))
 
             return position;
         }
 
-        private drawCube(cubePosition: Vector2) {
-            Setups.I.Draw.PolygonFill(<FillPolygonParams>{ color: new Color4(102, 153, 255, 1), position: cubePosition, points: this.pointsTop });
-            Setups.I.Draw.PolygonFill(<FillPolygonParams>{ color: new Color4(51, 119, 255, 1), position: cubePosition, points: this.pointsLeft });
-            Setups.I.Draw.PolygonFill(<FillPolygonParams>{ color: new Color4(255, 133, 102, 1), position: cubePosition, points: this.pointsRight });
+        private drawCube(cubePosition: Vector2, scale: number) {
+            Setups.I.Draw.PolygonFill(<FillPolygonParams>{ color: new Color4(102, 153, 255, 1), position: cubePosition, points: this.pointsTop, scale: scale });
+            Setups.I.Draw.PolygonFill(<FillPolygonParams>{ color: new Color4(51, 119, 255, 1), position: cubePosition, points: this.pointsLeft, scale: scale });
+            Setups.I.Draw.PolygonFill(<FillPolygonParams>{ color: new Color4(255, 133, 102, 1), position: cubePosition, points: this.pointsRight, scale: scale });
         }
 
-        private startMoving() {
-            this.proccess.GoTo(1, this.speed, this.endMoving);
+        private startMoving(those) {
+            those.proccess.GoTo(1, those.speed, () => {
+                those.endMoving(those);
+            });
         }
 
-        private endMoving() {
-            this.proccess.GoTo(0, this.speed, this.startMoving);
+        private endMoving(those) {
+            those.proccess.GoTo(0, those.speed, () => {
+                those.startMoving(those);
+            });
         }
     }
 }
